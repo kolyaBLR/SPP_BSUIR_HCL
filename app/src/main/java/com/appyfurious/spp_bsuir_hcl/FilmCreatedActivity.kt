957defdac38e14c2bb145ebd4ce063fa6ed12362
bobrovappyfurious <n.bobrov@appyfurious.com>
2018-06-09 16:43:26 +0300

    create item_film

--- app/src/main/java/com/appyfurious/spp_bsuir_hcl/FilmCreatedActivity.kt
+++ app/src/main/java/com/appyfurious/spp_bsuir_hcl/FilmCreatedActivity.kt
@@ -2,14 +2,11 @@ package com.appyfurious.spp_bsuir_hcl
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_video_created.*
 
-class VideoCreatedActivity : AppCompatActivity() {
+class FilmCreatedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_video_created)
-
-
+        setContentView(R.layout.activity_film_created)
     }
 }
--- app/src/main/java/com/appyfurious/spp_bsuir_hcl/ItemFilmHolder.kt
+++ app/src/main/java/com/appyfurious/spp_bsuir_hcl/ItemFilmHolder.kt
@@ -1,17 +1,12 @@
 package com.appyfurious.spp_bsuir_hcl
 
-import android.support.v7.widget.AppCompatSpinner
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 
 class ItemFilmHolder(val view: View) : RecyclerView.ViewHolder(view) {
-    val name = view.findViewById<EditText>(R.id.nameFilm)
-    val imageLink = view.findViewById<EditText>(R.id.linkImageFilm)
-    val tags = view.findViewById<EditText>(R.id.tagsFilm)
-    val video = view.findViewById<AppCompatSpinner>(R.id.filmVideoID)
-    val addButton = view.findViewById<Button>(R.id.addFilmButton)
-    val imageView = view.findViewById<ImageView>(R.id.filmImageView)
+    val name = view.findViewById<TextView>(R.id.itemNameFilm)!!
+    val imageView = view.findViewById<ImageView>(R.id.itemImageFilm)!!
+    val tags = view.findViewById<TextView>(R.id.itemTagsFilm)!!
 }
