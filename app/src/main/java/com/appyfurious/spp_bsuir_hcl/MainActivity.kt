7ce9416bef1d3a51a26f3d9814488608bc41c2d9
bobrovappyfurious <n.bobrov@appyfurious.com>
2018-06-09 13:55:33 +0300

    init

---
+++ app/src/main/java/com/appyfurious/spp_bsuir_hcl/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.appyfurious.spp_bsuir_hcl
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
