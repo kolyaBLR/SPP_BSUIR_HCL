fc7e91448ee1f09c75dc489961933010bb416746
Belikov Vlad <belickov.vlad1997@yandex.ru>
2018-06-11 16:26:51 +0300

    add piccasso

--- app/src/main/java/com/appyfurious/spp_bsuir_hcl/ui/adapter/ItemFilmAdapter.kt
+++ app/src/main/java/com/appyfurious/spp_bsuir_hcl/ui/adapter/ItemFilmAdapter.kt
@@ -2,21 +2,29 @@ package com.appyfurious.spp_bsuir_hcl.ui.adapter
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.appyfurious.spp_bsuir_hcl.R
 import com.appyfurious.spp_bsuir_hcl.db.Film
 import com.appyfurious.spp_bsuir_hcl.ui.adapter.holder.ItemFilmHolder
 
-class ItemFilmAdapter(val context: Context, val items : List<Film>) : RecyclerView.Adapter<ItemFilmHolder>() {
+class ItemFilmAdapter(val context: Context, val items: List<Film>) : RecyclerView.Adapter<ItemFilmHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemFilmHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(context).inflate(R.layout.item_film, parent, false)
+        return ItemFilmHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount() = items.size
 
     override fun onBindViewHolder(holder: ItemFilmHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val item = items[position]
+        holder.apply {
+            this.name.text = item.name
+            this.tags.text = item.tags.joinToString(", ")
+            item.imageLink?.let {
+
+            }
+        }
     }
 
 }
