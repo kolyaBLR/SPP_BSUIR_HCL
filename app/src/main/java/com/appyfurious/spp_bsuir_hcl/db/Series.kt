3168a1c409913f5e94ac33744c353e58cd0d1715
Belikov Vlad <belickov.vlad1997@yandex.ru>
2018-06-11 23:32:41 +0300

    add seasons, actors and producers

--- app/src/main/java/com/appyfurious/spp_bsuir_hcl/db/Series.kt
+++ app/src/main/java/com/appyfurious/spp_bsuir_hcl/db/Series.kt
@@ -1,5 +1,6 @@
 package com.appyfurious.spp_bsuir_hcl.db
 
+import io.realm.RealmList
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import java.util.*
@@ -10,4 +11,7 @@ open class Series : RealmObject() {
     var name: String? = null
     var description: String? = null
     var preview: String? = null
+    var seasons : RealmList<Season> = RealmList()
+    var actors: RealmList<Actor> = RealmList()
+    var producers: RealmList<Producer> = RealmList()
 }
