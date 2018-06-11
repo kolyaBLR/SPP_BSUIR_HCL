13ccb153bb57de3c44088530e0f52ae4bc909d0d
bobrovappyfurious <n.bobrov@appyfurious.com>
2018-06-11 23:47:02 +0300

    add menu

---
+++ app/src/main/java/com/appyfurious/spp_bsuir_hcl/ui/activity/FilmDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.appyfurious.spp_bsuir_hcl.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.appyfurious.spp_bsuir_hcl.R
+
+class FilmDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film_detail)
+    }
+}
